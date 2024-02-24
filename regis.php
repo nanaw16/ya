3e47f58db08f92170d060046b0cf9615b790f323
nanaw16 <159536532+nanaw16@users.noreply.github.com>
2024-02-24 15:16:26 +0700

    Add files via upload

---
+++ regis.php
@@ -0,0 +1,99 @@
+<?php
+include 'function.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Register Member</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="sb-admin/css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gradient-info">
+
+    
+    <div class="row justify-content-center">
+            <div class="col-md-5 mt-5">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+       
+                <!-- Nested Row within Card Body -->
+                <div class="p-5">
+							<div class="text-center">
+								<h4 class="h4 text-gray-900 mb-4">Registrasi Member</h1>
+                            </div>
+                            <form action="" method="POST">
+                            <div class="form-group">
+                                    <input type="text" name="nama" class="form-control" placeholder="nama" required>
+                                
+								</div>
+								<div class="form-group">
+                                    <input type="text" name="Username" class="form-control" placeholder="Username" required>
+                                
+								</div>
+								<div class="form-group">
+                                    <input type="Password" name="Password" class="form-control" placeholder="Password" required>
+                                </div>
+								
+                                <button type="submit" value="submit" class="btn btn-primary btn-block" name="submit">Registrasi</button>
+                                </from>
+         <?php
+            if(isset($_POST['submit'])){
+
+         
+		  $nama = $_POST['nama'];
+		  $Username = $_POST['Username'];
+		  $Password = $_POST['Password'];  
+
+          $insert = mysqli_query($conn, "INSERT INTO member VALUES (
+             null,
+					  '".$nama."',
+					  '".$Username."', 
+                      '".$Password."')
+            ");
+          if($insert){
+            echo '<script>alert("registrasi member berhasil")</script>';
+            echo '<script>window.location="login.php";</script>';
+          }else{
+            echo 'gagal '.mysqli_error($conn);
+          }
+        }
+        ?>
+                                <hr>
+                                
+                            <div class="text-center">
+                                <a class="small" href="login.php">Back to login!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="sb-admin/vendor/jquery/jquery.min.js"></script>
+    <script src="sb-admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="sb-admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="sb-admin/js/sb-admin-2.min.js"></script>
+</body>
+
+</html>
